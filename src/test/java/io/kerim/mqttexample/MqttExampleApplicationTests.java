58ad8e1736cf9505518aab7c34a6d8386bb44869
KerimAksak <abdulkerim.aksak@outlook.com>
2024-03-26 11:37:11 +0300

    init project

---
+++ src/test/java/io/kerim/mqttexample/MqttExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kerim.mqttexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
